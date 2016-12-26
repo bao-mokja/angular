319ea73a3905169ca1af31af04eed0e1afd2e9cf
Bao Nguyen <bao.mokja@gmail.com>
2016-12-26 06:29:57 +0000

    Assignment 1 - add color

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ TestFunction.$inject = ["$scope"];
 function TestFunction($scope){    
   $scope.list = "";
   $scope.message = "Please enter data first";
+  $scope.textColor = "red";
 
   $scope.displayMessage = function () {
-    var listOfItems = $scope.list.split(',')
+    var listOfItems = $scope.list.split(',').filter(String)
     if(listOfItems.length <= 3){
         $scope.message = "Enjoy!"
+        $scope.textColor = "green"
     }
     else if(listOfItems.length > 3){
         $scope.message = "Too much!"
+        $scope.textColor = "green"
     }
   };
 
