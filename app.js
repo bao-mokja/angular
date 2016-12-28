be608d61845cd4fe0b10dc2dcc00b73099cbc4de
Bao Nguyen <bao.mokja@gmail.com>
2016-12-28 08:45:13 +0000

    Assignment3

--- app.js
+++ app.js
@@ -81,23 +81,9 @@ function NarrowItDownController(MenuSearchService){
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath){
-    var service = this;
+    var service = this;   
     
     service.getMatchedMenuItems = function(searchTerm){
-         var promise = service.getMenuItems();
-         promise.then(function(menu_items){
-            var matchItems = [];
-            for(var i = 0; i < menu_items.length; i++){
-                //if(menu_items[i].description.indexOf(searchTerm) !== -1){
-                    matchItems.push(menu_items[i]);
-                //}
-            }
-            return matchItems;
-        });
-        return promise;
-    };
-    
-    service.getMenuItems = function(){
        var promise = $http({
             method: "GET",
             url: (ApiBasePath + "/menu_items.json")
@@ -105,6 +91,15 @@ function MenuSearchService($http, ApiBasePath){
         
         promise.then(function (response) {
             return response.data.menu_items;
+        })
+		.then(function(menu_items){
+            var matchItems = [];
+            for(var i = 0; i < menu_items.length; i++){
+                //if(menu_items[i].description.indexOf(searchTerm) !== -1){
+                    matchItems.push(menu_items[i]);
+                //}
+            }
+            return matchItems;
         })
         .catch(function (error) {
             console.log("Something went terribly wrong.");
