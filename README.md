9d26bb4ecca94b43fde1a0cfdebed882bbf486bc
Bao Nguyen <bao.mokja@gmail.com>
2016-12-28 05:08:02 +0000

    Assignment3

--- README.md
+++
@@ -1,2 +0,0 @@
-# angular
-angular learn repo
--- app.js
+++ app.js
@@ -1,58 +1,107 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyController', ToBuyController )
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController',NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('ApiPath', "http://davids-restaurant.herokuapp.com/menu_items.json");
 
-// LIST #1 - controller
-ToBuyController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyController (ShoppingListCheckOffService) {
-  var toBuyList = this;
-
-  toBuyList.items = ShoppingListCheckOffService.getToBuyItems();
-  
-  toBuyList.markAsBought = function(itemIndex){
-      ShoppingListCheckOffService.markAsBought(itemIndex);
-  }
+function FoundItemsDirective(){
+    var ddo = {
+        templateUrl: 'loader/itemsloaderindicator.template.html',
+        scope: {
+            items: '<',
+            onRemove: '&'
+        },
+        controller: FoundItemsDirectiveController,
+        controllerAs: 'list',
+        bindToController: true,
+        link: FoundItemsDirectiveLink
+    };
+    return ddo;
 }
 
-
-// LIST #2 - controller
-AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-function AlreadyBoughtController(ShoppingListCheckOffService) {
-  var boughtList = this;
-
-  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+function FoundItemsDirectiveLink(scope, element, attrs, controller) {
+    
+    scope.$watch('list.isListEmpty()', function(newValue, oldValue){
+        if (newValue === true) {
+            displayWarning();
+        }
+        else{
+            removeWarning();
+        }
+    });
+    
+    function displayWarning() {
+        var warningElem = element.find("div.warning");
+        warningElem.slideDown(900);
+    }
+    
+    function removeWarning() {
+        var warningElem = element.find("div.warning");
+        warningElem.slideUp(900);
+    }
 }
 
+function FoundItemsDirectiveController(){
+    
+    var list = this;
+    
+    list.isListEmpty = function(){
+        return list.items.length === 0;
+    };
+}
 
-function ShoppingListCheckOffService() {
-  var service = this;
-
-  var toBuyItems = [{ name: "cookies", quantity: 10 },
-                    { name: "chips", quantity: 10 },
-                    { name: "cokes", quantity: 10 },
-                    { name: "plates", quantity: 10 },
-                    { name: "forks", quantity: 10 },
-                    { name: "cups", quantity: 10 }];
-  var boughtItems = [];
-
-  service.getToBuyItems = function () {
-    return toBuyItems;
-  };
-  
-  service.getBoughtItems = function () {
-    return boughtItems;
-  };
-  
-  service.markAsBought = function(itemIndex){
-      var boughtItem = toBuyItems[itemIndex];
-      toBuyItems.splice(itemIndex,1);
-      boughtItems.push(boughtItem);
-  }
+NarrowItDownController.$inject = ['MenuSearchService']
+function NarrowItDownController(MenuSearchService){
+    var ctrl = this;
+    
+    ctrl.searchTerm = "";
+    
+    ctrl.warning = "";
+    
+    ctrl.narrowItDown = function(){
+        ctrl.foundItems = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+        if(ctrl.foundItems.length === 0){
+            displayWarning() ;
+        }
+        
+    }
+    
+    ctrl.removeItem = function(itemIndex){
+        ctrl.foundItems.splice(itemIndex,1);
+    }
 }
 
 
 })();
+
+MenuSearchService.$inject = ['$http', 'ApiPath'];
+function MenuSearchService($http, ApiPath){
+    var service = this;
+    
+    
+    service.getMatchedMenuItems = function(searchTerm){
+        var foundItems = [];
+        if(searchTerm === ""){
+            return foundItems;
+        }
+        var promise = $http({
+            method: "GET",
+            url: (ApiPath)
+        });
+        promise.then(function (response) {
+            var menuItems = response.data;
+            menuItems.forEach(function(entry){
+                if(entry.description.indexOf(searchTerm)!==-1){
+                    foundItems.push(entry);
+                }
+            });
+        })
+        .catch(function (error) {
+            console.log("Nothing found");
+        })
+        return foundItems;
+    }
+}
