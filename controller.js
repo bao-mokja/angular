e86fc22660f0f5c81e2cfb462ac6c143361491ea
Bao Nguyen <bao.mokja@gmail.com>
2016-12-29 06:05:38 +0000

    Assignment3

--- controller.js
+++ controller.js
@@ -2,8 +2,7 @@
 'use strict';
 
 angular.module('NarrowItDownApp')
-.controller('NarrowItDownController',NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.controller('NarrowItDownController',NarrowItDownController);
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
@@ -35,33 +34,6 @@ function NarrowItDownController(MenuSearchService){
     
     ctrl.removeItem = function(itemIndex){
         ctrl.foundItems.splice(itemIndex,1);
-    };
-}
-
-MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-function MenuSearchService($http, ApiBasePath){
-    var service = this;   
-    
-    service.getMatchedMenuItems = function(searchTerm){
-       return $http({
-            method: "GET",
-            url: (ApiBasePath + "/menu_items.json")
-        })
-		.then(function (response) {
-            return response.data.menu_items;
-        })
-		.then(function(menu_items){
-            var matchItems = [];
-            for(var i = 0; i < menu_items.length; i++){
-                if(menu_items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1){
-                    matchItems.push(menu_items[i]);
-                }
-            }
-            return matchItems;
-        })
-        .catch(function (error) {
-            console.log("Something went terribly wrong.");
-        });
     };
 }
 
