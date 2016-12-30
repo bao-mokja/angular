a5eeb0c7b280a20cfa8bae76319ed9936806730d
Bao Nguyen <bao.mokja@gmail.com>
2016-12-30 00:42:19 +0000

    adding routes

--- component.js
+++ component.js
@@ -4,11 +4,18 @@
 angular.module('NarrowItDownApp')
 .component('foundItems', {
     templateUrl: 'menuitems.html',    
+    controller: FoundItemsController,
     bindings: {
         items: '<',
         onRemove: '&'  
     }
 });
 
+function FoundItemsController(){
+    var $ctrl = this;
+    $ctrl.remove = function (myIndex) {
+        $ctrl.onRemove({ index: myIndex });
+    };
+}
 
 })();
