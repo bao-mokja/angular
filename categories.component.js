5ad28484c6574c984491b986f4847336bcf61f1d
Bao Nguyen <bao.mokja@gmail.com>
2017-01-03 04:05:01 +0000

    Assignment4

--- categories.component.js
+++ categories.component.js
@@ -3,7 +3,7 @@
 
 angular.module('MenuApp')
 .component('categories', {
-    templateUrl: 'categories.html',    
+    templateUrl: 'categories.template.html',    
     bindings: {
         items: '<'
     }
